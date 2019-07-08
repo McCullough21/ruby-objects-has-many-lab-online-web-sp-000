2a129caa1c8614b08572c4752fa4efb682b22c80
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-08 20:30:04 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Post
+
+attr_accessor :title, :author
+@@all = []
+
+def initialize(title)
+  @title = title
+  @@all << title
+end
+def self.all
+  @@all
+end
+
+def author_name
+  if post.author
+    post.author.name
+  else
+    nil
+  end
+end
+
+end
