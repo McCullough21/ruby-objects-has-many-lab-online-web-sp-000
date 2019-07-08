fca4c000dc8928c4e0c35a72697a79bb53d03c40
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-08 19:38:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,15 @@ class Artist
  attr_accessor :name, :songs, :genre
 
 def initialize(name)
-  
+  @name = name
+  @song = []
 end
+
+def add_song_by_name(name, genre)
+   song = Song.new
+   @song << song
+   song.artist = self
+end
+
 
 end
